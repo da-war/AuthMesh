3f29f2ccc799f3800965336335577750c03cb12d
drkast <imdawar090@gmail.com>
2024-09-22 05:44:06 +0500

    admin check middleware added

---
+++ src/middlewares/isAdmin.ts
@@ -0,0 +1,23 @@
+import { Request, Response, NextFunction } from 'express';
+import { sendErrorRes } from '../../src/utils/helper';
+import User from '../../src/model/user';
+
+export const isAdmin = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+        // Ensure the user is authenticated and has admin privileges
+        if (!req.user || !req.user.id) {
+            return sendErrorRes(res, 'Unauthorized request', 403);
+        }
+        // Fetch the user from the database
+        const user = await User.findById(req.user.id);
+
+        if (!user || !user.isAdmin) {
+            return sendErrorRes(res, 'Forbidden: Admins only', 403);
+        }
+
+        // If user is an admin, proceed
+        next();
+    } catch (error) {
+        next(error);
+    }
+};
